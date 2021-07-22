116d644b4630178f3178c9d174b8088df00044bf
AndrejKh <silverwewel@gmail.com>
2021-07-22 16:38:57 +0200

    Update reservation_model.php

--- application/models/reservation_model.php
+++ application/models/reservation_model.php
@@ -160,7 +160,7 @@ class Reservation_model extends CI_Model
             $mail->AltBody = $content;
 
             $mail->send();
-            echo 'Message has been sent';
+            // echo 'Message has been sent';
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
