5adc1797e11ffe983e9277c62394ad71eb707b92
AndrejKh <silverwewel@gmail.com>
2021-07-23 13:07:23 +0200

    delete link update

--- application/views/events/list.php
+++ application/views/events/list.php
@@ -47,10 +47,9 @@
 </svg>&nbsp;  Reservation List</a>
                 </li>
                 <li>
-                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                   <?= anchor(base_url() .  "events/delete/" . $event['idevents'], '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
   <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/>
-</svg> &nbsp; 
-                  <?= anchor(base_url() .  "events/delete/" . $event['idevents'], "Delete", array('onclick' => "return confirm('Do you want delete this event?!')", 'class' => 'dropdown-item text-secondary')) ?>
+</svg> Delete', array('onclick' => "return confirm('Do you want delete this event?!')", 'class' => 'dropdown-item text-secondary')) ?>
                 </li>
               </ul>
             </div>
