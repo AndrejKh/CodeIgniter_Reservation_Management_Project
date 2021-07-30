733a0d71c6edfd59720a41c00c1effd39ebdf184
AndrejKh <silverwewel@gmail.com>
2021-07-30 12:35:10 +0200

    fix reservation form

--- application/views/reservations/create_public.php
+++ application/views/reservations/create_public.php
@@ -1,5 +1,13 @@
 <div class="row pt-5">
     <div class="col-md-6 offset-md-3 event-form">
+            <div class="card" style="width: 27rem; margin: 20px auto; border: 0 !important;">
+                <img src="<?= base_url() ?>/assets/images/events/<?= $event['image'] ?>" style="width: 100%; height: 100%;max-height: 300px;" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text text-center mb-0" style="font-size: 18px; ">Data: <span class="fw-bold text-capitalize"> <?= $event['date'] ?></span> </p>
+                        <p class="card-text text-center" style="font-size: 22px;">Te ftuar: <span class="fw-bold text-capitalize"> <?= $event['guest'] ?></span> </p>
+                    </div>
+            </div>
+
         <h2 class="text-center fw-bold"><?= $title; ?></h2>
 
         <?php echo validation_errors(); ?>
@@ -8,17 +16,17 @@
 
 
         <div class="form-group mb-3">
-            <label>Name</label>
+            <label>Emri</label>
             <input type="text" class="form-control" required name="name" placeholder="Add name">
         </div>
 
         <div class="form-group mb-3">
-            <label>Last name</label>
+            <label>Mbiemri</label>
             <input type="text" class="form-control" required name="last_name" placeholder="Add last  name">
         </div>
 
         <div class="form-group mb-3">
-            <label>Phone</label>
+            <label>Nr telefoni</label>
             <input type="tel" class="form-control" required name="phone_nr" placeholder="Add phone">
         </div>
 
@@ -28,7 +36,7 @@
         </div>
 
         <div class="form-group mb-3">
-            <label>Total persons </label>
+            <label>Nr i personave</label>
             <input type="number" class="form-control" required name="total_persons" placeholder="Add total" min="1">
         </div>
 
@@ -42,7 +50,7 @@
         <?php } ?>
 
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Dergo</button>
         </form>
     </div>
 </div>
